771fda751c4005485dca928bdb404fdc8df3b502
Roland Tanglao <roland@rolandtanglao.com>
2013-04-21 21:45:30 -0700

    crude attempt at rendering 40,000 75x75 vancouver photos

---
+++ webshot.roland.vancouver.flickr.2004.2012.js
@@ -0,0 +1,5 @@
+var webshot = require('webshot');
+
+webshot('https://dl.dropboxusercontent.com/u/361757/ROLAND_VANCOUVER_FLICKR/roland.vancouver.75x75.2004.2012.html', 'roland.vancouver.2004.2012.flickr.png', function(err) {
+  // screenshot now saved to google.png 
+});
