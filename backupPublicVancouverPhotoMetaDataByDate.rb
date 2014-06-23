75d101d984e296d8eee91462251a888506c5f024
Roland Tanglao <roland@rolandtanglao.com>
2014-06-23 16:13:07 -0700

    add license and description fields, will have to re-run for feb 27, 2004 and earlier

--- backupPublicVancouverPhotoMetaDataByDate.rb
+++ backupPublicVancouverPhotoMetaDataByDate.rb
@@ -36,7 +36,7 @@ if MONGO_USER
   end
 end
 
-extras_str = "date_upload, date_taken, owner_name,"+
+extras_str = "description, license, date_upload, date_taken, owner_name,"+
              "icon_server, original_format, last_update, geo, tags,"+
              " machine_tags, o_dims, views, media, path_alias, url_sq,"+
              "url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o"
