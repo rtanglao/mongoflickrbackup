de834ae14a240630d830ce8cef89c1e517313458
Roland Tanglao <roland@rolandtanglao.com>
2014-07-02 22:01:21 -0700

    add a script to list photos between a certain date

---
+++ list75x75ByDate.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+require 'curb'
+require 'pp'
+require 'time'
+require 'date'
+require 'mongo'
+require 'parseconfig'
+require 'uri'
+
+if ARGV.length < 6
+  puts "usage: #{$0} yyyy mm dd yyyy mmm dd -v"
+  exit
+end
+
+MIN_DATE = Time.local(ARGV[0].to_i, ARGV[1].to_i, ARGV[2].to_i, 0, 0) # may want Time.utc if you don't want local time
+MAX_DATE = Time.local(ARGV[3].to_i, ARGV[4].to_i, ARGV[5].to_i, 23, 59) # may want Time.utc if you don't want local time
+
+
+MONGO_HOST = ENV["MONGO_HOST"]
+raise(StandardError,"Set Mongo hostname in ENV: 'MONGO_HOST'") if !MONGO_HOST
+MONGO_PORT = ENV["MONGO_PORT"]
+raise(StandardError,"Set Mongo port in ENV: 'MONGO_PORT'") if !MONGO_PORT
+MONGO_USER = ENV["MONGO_USER"]
+# raise(StandardError,"Set Mongo user in ENV: 'MONGO_USER'") if !MONGO_USER
+MONGO_PASSWORD = ENV["MONGO_PASSWORD"]
+# raise(StandardError,"Set Mongo user in ENV: 'MONGO_PASSWORD'") if !MONGO_PASSWORD
+FLICKR_DB = ENV["FLICKR_DB"]
+raise(StandardError,"Set Mongo flickr database name in ENV: 'FLICKR_DB'") if !FLICKR_DB
+FLICKR_USER = ENV["FLICKR_USER"]
+raise(StandardError,"Set flickr user name in ENV: 'FLICKR_USER'") if !FLICKR_USER
+
+db = Mongo::Connection.new(MONGO_HOST, MONGO_PORT.to_i).db(FLICKR_DB)
+if MONGO_USER
+  auth = db.authenticate(MONGO_USER, MONGO_PASSWORD)
+  if !auth
+    raise(StandardError, "Couldn't authenticate, exiting")
+    exit
+  end
+end
+
+photosColl = db.collection("photos")
+
+
+photosColl.find({"datetaken" =>
+                  {"$gte" => MIN_DATE, "$lte" => MAX_DATE}},
+                  :fields => ["datetaken", "id"]
+                ).sort([["datetaken", Mongo::ASCENDING]]).each do |p|
+  $stderr.printf("photo:%d, datetaken:%s\n", p["id"], p["datetaken"].to_s)
+end
+
+
+
